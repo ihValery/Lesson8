780a8d77d38bb227bee361ad4df397fb7d70a9ab
ihValery <ihValery@gmail.com>
2021-01-29 19:25:18 +0300

    Осталось логику доработать и При перетягивании слайдера меняется значение у Progress View и TextField При внесении изменений в TextField с помощью клавиатуры - меняем значения у Progress View и Slider

--- HomeWorkLesson8/ViewController.swift
+++ HomeWorkLesson8/ViewController.swift
@@ -19,27 +19,78 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setAlarmButton.setTitleColor(.black, for: .normal)
+        setAlarmButton.setTitleColor(.systemPurple, for: .normal)
         setAlarmButton.backgroundColor = .systemPurple
         setAlarmButton.layer.cornerRadius = 13
         
-        deletAlarmButton.setTitleColor(.black, for: .normal)
+        deletAlarmButton.setTitleColor(.systemPurple, for: .normal)
         deletAlarmButton.backgroundColor = .systemPurple
         deletAlarmButton.layer.cornerRadius = 13
+        deletAlarmButton.isEnabled = false
         
-        isHiddenSettingAlarm(myBool: true)
+        volumeProgressView.trackTintColor = .lightGray
+        volumeProgressView.progressTintColor = .systemPurple
+        volumeSlider.tintColor = .systemPurple
+        switchOnOffAlarm.onTintColor = .systemPurple
+        volumeTextField.placeholder = "0.0"
+        
+        isHiddenSettingAlarm(bool: true)
+        switchOneAlarm(bool: true)
         // Do any additional setup after loading the view.
     }
+    var timeStringForLabel = ""
+    
+    //Нажимаем кнопку Установть
     @IBAction func setAlarmButtonAction(_ sender: UIButton) {
-        isHiddenSettingAlarm(myBool: false)
+        isHiddenSettingAlarm(bool: false)
+        setTimeLabel.text = timeStringForLabel
+        deletAlarmButton.isEnabled = true
+        deletAlarmButton.setTitleColor(.black, for: .normal)
+        switchOneAlarm(bool: false)
+
     }
     
-    func isHiddenSettingAlarm(myBool: Bool) {
-        setTimeLabel.isHidden = myBool
-        switchOnOffAlarm.isHidden = myBool
-        volumeProgressView.isHidden = myBool
-        volumeSlider.isHidden = myBool
-        volumeTextField.isHidden = myBool
+    @IBAction func deletAlarmButtonAction(_ sender: Any) {
+        setTimeLabel.text = ""
+        isHiddenSettingAlarm(bool: true)
+        switchOneAlarm(bool: true)
+        deletAlarmButton.isEnabled = false
+        deletAlarmButton.setTitleColor(.systemPurple, for: .normal)
+    }
+    
+    @IBAction func switchAction(_ sender: UISwitch) {
+        if volumeProgressView.isHidden {
+            switchOneAlarm(bool: false)
+            setTimeLabel.alpha = 1
+        } else {
+            switchOneAlarm(bool: true)
+            setTimeLabel.alpha = 0.3
+        }
+    }
+    
+    @IBAction func sliderChanger(_ sender: UISlider) {
+        
+    }
+    
+    //Крутим барабан. На прямую присвоить нельзя. Запоминаем работаем
+    @IBAction func changeTimeDatePicker(_ sender: UIDatePicker) {
+        setAlarmButton.setTitleColor(.black, for: .normal)
+        
+        let timeFormatter = DateFormatter()
+        
+        timeFormatter.timeStyle = .short
+        timeStringForLabel = timeFormatter.string(from: sender.date)
+    }
+    
+    func isHiddenSettingAlarm(bool: Bool) {
+        setTimeLabel.isHidden = bool
+        switchOnOffAlarm.isHidden = bool
+    }
+    
+    func switchOneAlarm(bool: Bool) {
+        volumeProgressView.isHidden = bool
+        volumeSlider.isHidden = bool
+        volumeTextField.isHidden = bool
     }
 }
 
